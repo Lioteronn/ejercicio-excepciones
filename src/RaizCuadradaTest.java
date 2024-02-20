1b62b51cb5c066d0553a95e12a64f836c0ab6316
Lioteron <lioteronex@gmail.com>
2024-02-20 12:39:42 +0100

    Added csv files.

--- src/RaizCuadradaTest.java
+++ src/RaizCuadradaTest.java
@@ -7,14 +7,14 @@ class RaizCuadradaTest {
 
     // Test con valores de entrada válidos
     @ParameterizedTest
-    @CsvFileSource(resources = "csv/ValoresValidos.csv", numLinesToSkip = 1, delimiter = ';')
+    @CsvFileSource(resources = "csv/ValoresValidos.csv", numLinesToSkip = 1, delimiter = ';') // Archivo csv
     void raizCuadrada(double numero, double indice, double resultadoEsperado) {
         assertEquals(String.valueOf(resultadoEsperado), Main.raizCuadrada(numero, indice));
     }
 
     // Test con valores de entrada inválidos
     @ParameterizedTest
-    @CsvFileSource(resources = "csv/ValoresInvalidos.csv", numLinesToSkip = 1, delimiter = ';')
+    @CsvFileSource(resources = "csv/ValoresInvalidos.csv", numLinesToSkip = 1, delimiter = ';') // Archivo csv
     void raizCuadradaExcepcion(double numero, double indice, String resultadoEsperado) {
         assertEquals(String.valueOf(resultadoEsperado), Main.raizCuadrada(numero, indice));
     }
